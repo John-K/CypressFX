e920ff6121ff6e56e2687a7688fefdfb4b284868
John Kelley <john@kelley.ca>
2017-08-27 01:10:38 -0700

    Fix libusb issue #110, ctrl_transfer cannot be passed > 4096 bytes at a time

--- CypressFX/__init__.py
+++ CypressFX/__init__.py
@@ -6,12 +6,15 @@ import usb.util
 import intelhex
 import pkg_resources
 
+
 class FX2(object):
     """Supports firmware and EEPROM operations on Cypress FX2 devices"""
     REQ_WRITE = 0x40
     REQ_READ = 0xC0
     CMD_RW_INTERNAL = 0xA0
     CMD_RW_EEPROM = 0xA2
+    MAX_CTRL_BUFFER_LENGTH = 4096
+
     def __init__(self, usbDev):
         if not usbDev:
             raise AttributeError("USB Device passed is not valid")
@@ -37,39 +40,54 @@ class FX2(object):
     def reset(self, enable_cpu):
         """Resets a device and optionally enables the CPU core"""
         cpu_address = 0xE600
-        self.dev.ctrl_transfer(self.REQ_WRITE, self.CMD_RW_INTERNAL, cpu_address, 0x00,
-                               enable_cpu)
+        self.dev.ctrl_transfer(self.REQ_WRITE, self.CMD_RW_INTERNAL,
+                               cpu_address, 0x00, enable_cpu)
 
     def __ensure_vend_ax_firmware(self):
         """Makes sure that we're running the default code"""
         if not self.running_vend_ax_fw:
-            hexfile = pkg_resources.resource_filename('CypressFX', 'vend_ax.hex')
+            hexfile = pkg_resources.resource_filename('CypressFX',
+                                                      'vend_ax.hex')
             self.load_intelhex_firmware(hexfile)
             self.running_vend_ax_fw = True
 
     def read_eeprom(self, length=8):
         """Reads bytes from the device's EEPROM"""
         self.__ensure_vend_ax_firmware()
-        data = self.dev.ctrl_transfer(self.REQ_READ, self.CMD_RW_EEPROM, 0x00, 0x00, length)
+        data = self.dev.ctrl_transfer(self.REQ_READ, self.CMD_RW_EEPROM, 0x00,
+                                      0x00, length)
         return data
 
     def write_eeprom(self, data):
         """Writes data to the device's EEPROM"""
         self.__ensure_vend_ax_firmware()
-        wrote = self.dev.ctrl_transfer(self.REQ_WRITE, self.CMD_RW_EEPROM, 0x00, 0x00, data)
+        wrote = self.dev.ctrl_transfer(self.REQ_WRITE, self.CMD_RW_EEPROM,
+                                       0x00, 0x00, data)
         return wrote
 
     def load_intelhex_firmware(self, filename):
         """Loads firmware from an IntelHex formatted file"""
-        total=0
+        total = 0
         fw_hex = intelhex.IntelHex(filename)
         self.reset(enable_cpu=False)
         for seg_start, seg_end in fw_hex.segments():
             data = fw_hex.tobinstr(start=seg_start, end=seg_end-1)
-            wrote = self.dev.ctrl_transfer(self.REQ_WRITE, self.CMD_RW_INTERNAL, seg_start, 0x00,
-                                           data)
-            total += wrote
-            if not wrote == len(data):
-                raise IOError("Failed to write %d bytes to %x" % (len(data), seg_start))
+            # libusb issue #110 https://github.com/libusb/libusb/issues/110
+            offset = 0
+            while len(data) > 0:
+                end = len(data)
+                if end > self.MAX_CTRL_BUFFER_LENGTH:
+                    end = self.MAX_CTRL_BUFFER_LENGTH
+                wrote = self.dev.ctrl_transfer(self.REQ_WRITE,
+                                               self.CMD_RW_INTERNAL,
+                                               seg_start+offset, 0x00,
+                                               data[:end])
+                if not wrote == end:
+                    raise IOError("Failed to write %d bytes to %x" % (end,
+                                  seg_start))
+                total += wrote
+                offset += wrote
+                data = [end:]
+
         self.reset(enable_cpu=True)
         return total
