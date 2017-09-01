51cda0896e7e720b0ce4dc88d8abd32a7ca2a6bd
John Kelley <john@kelley.ca>
2017-09-01 02:09:53 -0700

    Bump version to 0.4 for release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ name = 'CypressFX'
 desc = 'Python module to program Cypress FX EZ-USB series chipsets'
 long_desc = ''
 
-version = '0.3'
+version = '0.4'
 
 classifiers = [
     'Development Status :: 4 - Beta',
